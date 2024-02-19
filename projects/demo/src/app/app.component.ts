6e47f0151038d684b496be6e36c39fcb35a69635
Gábor Nagy <gabor.nagy@icellmobilsoft.hu>
2024-02-19 18:46:39 +0100

    simple demo page for the button widget

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Appearance, AppearanceType, IdsButtonComponent, Size, SizeType, Variant, VariantType } from '@i-cell/widgets/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IdsButtonComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'demo';
+  public appearances = Object.values(Appearance) as AppearanceType[];
+  public sizes = Object.values(Size) as SizeType[];
+  public variants = Object.values(Variant) as VariantType[];
+  public filteredVariants = Object.values(Variant).filter((variant) => !["error", "success", "warning"].includes(variant)) as VariantType[];
 }

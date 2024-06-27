69a299830c3742d4d27416a6b133005b28d79652
László Bátori <laszlo.batori@icellmobilsoft.hu>
2024-06-27 12:26:09 +0200

    hostClassGenerator fix

--- projects/widgets/core/utils/class-prefix.ts
+++ projects/widgets/core/utils/class-prefix.ts
@@ -2,10 +2,9 @@ export function addClassPrefix(componentClass: string, className: string | null)
   return className ? `${componentClass}-${className}` : null;
 }
 
-export function hostClassGenerator(componentClass: string, classNames: Array<string | null>, disabled?: boolean): string {
+export function hostClassGenerator(componentClass: string, classNames: Array<string | null> = []): string {
   return [
     componentClass,
     ...classNames.map((className) => addClassPrefix(componentClass, className)),
-    ...[(disabled === true) ? [addClassPrefix(componentClass, 'disabled')] : []],
   ].filter(Boolean).join(' ');
 }

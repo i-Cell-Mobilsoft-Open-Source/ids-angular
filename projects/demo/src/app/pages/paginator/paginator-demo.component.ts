afd08107fea8589706027450f09dafec27152e41
László Bátori <laszlo.batori@icellmobilsoft.hu>
2024-07-17 17:27:32 +0200

    Paginator: demo

--- projects/demo/src/app/pages/paginator/paginator-demo.component.ts
+++ projects/demo/src/app/pages/paginator/paginator-demo.component.ts
@@ -1,12 +1,33 @@
+import { UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { Size, SizeType } from '@i-cell/ids-angular/core';
+import { IdsPaginatorComponent, PaginatorVariant, PaginatorVariantType } from '@i-cell/ids-angular/paginator';
 
 @Component({
   selector: 'app-paginator-demo',
   standalone: true,
-  imports: [],
+  imports: [
+    IdsPaginatorComponent,
+    UpperCasePipe,
+  ],
   templateUrl: './paginator-demo.component.html',
   styleUrl: './paginator-demo.component.scss',
 })
 export class PaginatorDemoComponent {
+  public sizes: SizeType[] = [
+    Size.DENSE,
+    Size.COMPACT,
+    Size.COMFORTABLE,
+    Size.SPACIOUS,
+  ];
 
+  public variants: PaginatorVariantType[] = [
+    PaginatorVariant.PRIMARY,
+    PaginatorVariant.SECONDARY,
+    PaginatorVariant.LIGHT,
+    PaginatorVariant.SURFACE,
+  ];
+
+  // eslint-disable-next-line no-magic-numbers
+  public length = 80;
 }

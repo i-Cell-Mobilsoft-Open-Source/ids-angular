f9aee5a93b8d1c970016e14656d9fb2f0a966398
László Bátori <laszlo.batori@icellmobilsoft.hu>
2024-07-23 16:19:11 +0200

    Segmented control: package

---
+++ projects/widgets/segmented-control/public-api.ts
@@ -0,0 +1,5 @@
+export * from './ids-segmented-control-default-options';
+export * from './ids-segmented-control.directive';
+export * from './segmented-control-item/ids-segmented-control-item.component';
+export * from './types/ids-semneted-control-appearance';
+export * from './types/ids-semneted-control-variant';

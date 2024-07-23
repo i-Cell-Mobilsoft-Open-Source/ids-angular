e4934151930cfa15d466dbbf8b62cd129130699c
László Bátori <laszlo.batori@icellmobilsoft.hu>
2024-07-23 16:18:23 +0200

    Segmented control: improved demo

--- projects/demo/src/app/pages/segmented-control/segmented-control-demo.component.ts
+++ projects/demo/src/app/pages/segmented-control/segmented-control-demo.component.ts
@@ -1,6 +1,12 @@
+import { IdsIconComponent } from '../../../../../widgets/icon/ids-icon.component';
+
+import { UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { Size, SizeType } from '@i-cell/ids-angular/core';
+import { SegmentedControlAppearance, SegmentedControlAppearanceType, SegmentedControlVariant, SegmentedControlVariantType } from '@i-cell/ids-angular/segmented-control';
 import { IdsSegmentedControlDirective } from '@i-cell/ids-angular/segmented-control/ids-segmented-control.directive';
 import { IdsSegmentedControlItemComponent } from '@i-cell/ids-angular/segmented-control/segmented-control-item/ids-segmented-control-item.component';
+import { mdiAccount, mdiAlarm, mdiLightbulbOnOutline } from '@mdi/js';
 
 @Component({
   selector: 'app-segmented-control-demo',
@@ -8,10 +14,35 @@ import { IdsSegmentedControlItemComponent } from '@i-cell/ids-angular/segmented-
   imports: [
     IdsSegmentedControlDirective,
     IdsSegmentedControlItemComponent,
+    IdsIconComponent,
+    UpperCasePipe,
   ],
   templateUrl: './segmented-control-demo.component.html',
   styleUrl: './segmented-control-demo.component.scss',
 })
 export class SegmentedControlDemoComponent {
+  public icon = {
+    lightbulb: mdiLightbulbOnOutline,
+    account: mdiAccount,
+    alarm: mdiAlarm,
+  };
 
+  public sizes: SizeType[] = [
+    Size.DENSE,
+    Size.COMPACT,
+    Size.COMFORTABLE,
+    Size.SPACIOUS,
+  ];
+
+  public variants: SegmentedControlVariantType[] = [
+    SegmentedControlVariant.PRIMARY,
+    SegmentedControlVariant.SURFACE,
+    SegmentedControlVariant.LIGHT,
+    SegmentedControlVariant.DARK,
+  ];
+
+  public appearances: SegmentedControlAppearanceType[] = [
+    SegmentedControlAppearance.FILLED,
+    SegmentedControlAppearance.OUTLINED,
+  ];
 }

f9f17e7abe9dddfe7a563fa98f842e399e7aa96a
László Bátori <laszlo.batori@icellmobilsoft.hu>
2024-08-14 08:53:56 +0200

    feat(form-field): demo

---
+++ projects/demo/src/app/pages/form-field/form-field-demo.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IdsActionDirective, IdsErrorMessageComponent, IdsFormFieldComponent, IdsHintMessageComponent, IdsLabelDirective, IdsPrefixDirective, IdsSuffixDirective } from '@i-cell/ids-angular/forms';
+import { IdsInputDirective } from '@i-cell/ids-angular/forms/components/input/ids-input.directive';
+import { IdsIconComponent } from '@i-cell/ids-angular/icon';
+import { IdsIconButtonComponent } from '@i-cell/ids-angular/icon-button';
+import { mdiMagnify, mdiMoonWaningCrescent, mdiWhiteBalanceSunny } from '@mdi/js';
+
+@Component({
+  selector: 'app-form-field-demo',
+  standalone: true,
+  imports: [
+    IdsFormFieldComponent,
+    IdsLabelDirective,
+    IdsInputDirective,
+    IdsPrefixDirective,
+    IdsSuffixDirective,
+    IdsActionDirective,
+    IdsIconButtonComponent,
+    IdsIconComponent,
+    IdsHintMessageComponent,
+    IdsErrorMessageComponent,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './form-field-demo.component.html',
+  styleUrl: './form-field-demo.component.scss',
+})
+export class FormFieldDemoComponent {
+  public iconSun = mdiWhiteBalanceSunny;
+  public iconMoon = mdiMoonWaningCrescent;
+  public searchIcon = mdiMagnify;
+}

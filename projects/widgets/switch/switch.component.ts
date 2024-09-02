c6a54c39584581c9530e4bdc4b6151def77d944d
László Bátori <laszlo.batori@icellmobilsoft.hu>
2024-09-02 12:00:42 +0200

    feat(switch): base files

---
+++ projects/widgets/switch/switch.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ids-switch',
+  standalone: true,
+  imports: [],
+  templateUrl: './switch.component.html',
+  styleUrl: './switch.component.scss',
+})
+export class IdsSwitchComponent {
+
+}

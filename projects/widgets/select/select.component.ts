18d8c9b42282e9e6b096c6601005c3e0660d0857
László Bátori <laszlo.batori@icellmobilsoft.hu>
2024-09-04 10:34:49 +0200

    feat(select): base files

---
+++ projects/widgets/select/select.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ids-select',
+  standalone: true,
+  imports: [],
+  templateUrl: './select.component.html',
+  styleUrl: './select.component.scss',
+})
+export class IdsSelectComponent {
+
+}

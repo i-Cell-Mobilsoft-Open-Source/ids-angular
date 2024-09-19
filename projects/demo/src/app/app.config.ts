2de41ab914e0d0b2391a814438f8a3b712286bc9
Árpád Soós <arpad.soos@icellmobilsoft.hu>
2024-09-19 12:33:01 +0200

    feat(icon): implemented icon component

--- projects/demo/src/app/app.config.ts
+++ projects/demo/src/app/app.config.ts
@@ -4,12 +4,17 @@ import { HttpClient, provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
+import { IDS_ICON_DEFAULT_CONFIG, IdsIconDefaultOptions } from '@i-cell/ids-angular/icon';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export function HttpLoaderFactory(httpClient: HttpClient): TranslateHttpLoader {
   return new TranslateHttpLoader(httpClient);
 }
+
+const iconDefaultConfig: IdsIconDefaultOptions = {
+  iconAssetsPath: 'assets/icons',
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -25,5 +30,6 @@ export const appConfig: ApplicationConfig = {
       }),
     ),
     provideAnimations(),
+    { provide: IDS_ICON_DEFAULT_CONFIG, useValue: iconDefaultConfig },
   ],
 };

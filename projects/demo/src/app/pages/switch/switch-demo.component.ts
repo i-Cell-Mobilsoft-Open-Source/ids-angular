45f6169c632ec0737cb2737fee855df7a73266c2
László Bátori <laszlo.batori@icellmobilsoft.hu>
2024-09-30 14:27:20 +0200

    feat(demo): switch

--- projects/demo/src/app/pages/switch/switch-demo.component.ts
+++ projects/demo/src/app/pages/switch/switch-demo.component.ts
@@ -2,8 +2,23 @@ import { UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Size, SizeType } from '@i-cell/ids-angular/core';
-import { IdsSwitchComponent, IdsSwitchGroupComponent, SwitchVariant, SwitchVariantType } from '@i-cell/ids-angular/switch';
+import { IdsSwitchComponent, IdsSwitchGroupComponent, SwitchIconPosition, SwitchIconPositionType, SwitchLabelPosition, SwitchLabelPositionType, SwitchVariant, SwitchVariantType } from '@i-cell/ids-angular/switch';
 import { TranslateModule } from '@ngx-translate/core';
+
+type SwitchPublicApi = {
+  label: string,
+  required: boolean,
+  readonly: boolean,
+  size: SizeType,
+  variant: SwitchVariantType,
+  hasIcon: boolean,
+  iconPosition: SwitchIconPositionType,
+  labelPosition: SwitchLabelPositionType,
+  disabled: boolean,
+  ariaLabel: string,
+  ariaLabelledBy: string,
+  ariaDescribedBy: string,
+};
 
 @Component({
   selector: 'app-switch-demo',
@@ -16,19 +31,29 @@ import { TranslateModule } from '@ngx-translate/core';
     FormsModule,
   ],
   templateUrl: './switch-demo.component.html',
-  styleUrl: './switch-demo.component.scss',
+  styleUrls: [
+    '../demo-page.scss',
+    './switch-demo.component.scss',
+  ],
 })
 export class SwitchDemoComponent {
-  public sizes: SizeType[] = [
-    Size.DENSE,
-    Size.COMPACT,
-    Size.COMFORTABLE,
-    Size.SPACIOUS,
-  ];
+  public model: SwitchPublicApi = {
+    label: 'Switch label',
+    required: false,
+    readonly: false,
+    size: Size.COMPACT,
+    variant: SwitchVariant.SURFACE,
+    hasIcon: true,
+    iconPosition: SwitchIconPosition.ONHANDLE,
+    labelPosition: SwitchLabelPosition.RIGHT,
+    disabled: false,
+    ariaLabel: '',
+    ariaLabelledBy: '',
+    ariaDescribedBy: '',
+  };
 
-  public variants: SwitchVariantType[] = [
-    SwitchVariant.SURFACE,
-    SwitchVariant.LIGHT,
-    SwitchVariant.PRIMARY,
-  ];
+  public sizes = Object.values(Size) as SizeType[];
+  public variants = Object.values(SwitchVariant) as SwitchVariantType[];
+  public iconPositions = Object.values(SwitchIconPosition) as SwitchIconPositionType[];
+  public labelPositions = Object.values(SwitchLabelPosition) as SwitchLabelPositionType[];
 }

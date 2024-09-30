9c374948d4a6b91702715a9b0c6a5d28dcfa1683
László Bátori <laszlo.batori@icellmobilsoft.hu>
2024-09-30 16:15:16 +0200

    feat(demo): snackbar

--- projects/demo/src/app/pages/snackbar/snackbar-demo.component.ts
+++ projects/demo/src/app/pages/snackbar/snackbar-demo.component.ts
@@ -1,18 +1,28 @@
-import { Component, inject, OnInit, ViewContainerRef } from '@angular/core';
+import { Component, computed, inject, OnInit, ViewContainerRef } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IdsButtonComponent } from '@i-cell/ids-angular/button';
-import { IdsSnackbarAction, IdsSnackbarService, SnackbarVariant, SnackbarVariantType } from '@i-cell/ids-angular/snackbar';
+import { Size, SizeType } from '@i-cell/ids-angular/core';
+import { IdsSnackbarAction, IdsSnackbarService, SnackbarPosition, SnackbarPositionType, SnackbarVariant, SnackbarVariantType } from '@i-cell/ids-angular/snackbar';
 import { TranslateModule } from '@ngx-translate/core';
 
-const snackbarTexts: string[] = [
-  'Lorem, ipsum dolor.',
-  'Lorem ipsum dolor sit amet, consectetur adipisicing.',
-  'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic assumenda architecto officia?',
-  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nisi, quasi blanditiis enim consectetur quibusdam quos.',
-  // eslint-disable-next-line @stylistic/js/max-len
-  'Lorem ipsum dolor sit amet consectetur adipisicing elit. A mollitia excepturi magnam repellendus iste, corrupti delectus, quasi voluptate aut dignissimos praesentium natus similique iusto.',
-];
+type SnackbarPublicApi = {
+  message: string,
+  variant: SnackbarVariantType,
+  icon: string | undefined,
+  allowDismiss: boolean,
+  closeButtonLabel: string | undefined,
+  autoClose: boolean,
+  urgent: boolean,
+};
 
-let snackbarTextIndex = 0;
+type SnackbarHelperControls = {
+  useAction: boolean,
+  size: SizeType,
+  position: SnackbarPositionType,
+  newestAtStartPosition: boolean
+  viewportMargin: number
+  useActualViewContainer: boolean
+};
 
 @Component({
   selector: 'app-snackbar-demo',
@@ -20,41 +30,64 @@ let snackbarTextIndex = 0;
   imports: [
     IdsButtonComponent,
     TranslateModule,
+    FormsModule,
   ],
   templateUrl: './snackbar-demo.component.html',
-  styleUrl: './snackbar-demo.component.scss',
+  styleUrls: [
+    '../demo-page.scss',
+    './snackbar-demo.component.scss',
+  ],
 })
 export class SnackbarDemoComponent implements OnInit {
+  public model: SnackbarPublicApi & SnackbarHelperControls = {
+    message: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+    variant: SnackbarVariant.DARK,
+    icon: undefined,
+    allowDismiss: false,
+    closeButtonLabel: undefined,
+    autoClose: false,
+    urgent: false,
+    useAction: false,
+    size: Size.COMFORTABLE,
+    position: SnackbarPosition.BOTTOM_CENTER,
+    newestAtStartPosition: false,
+    viewportMargin: 16,
+    useActualViewContainer: true,
+  };
+
+  public sizes = Object.values(Size) as SizeType[];
+  public variants = Object.values(SnackbarVariant) as SnackbarVariantType[];
+  public positions = Object.values(SnackbarPosition) as SnackbarPositionType[];
+
   private readonly _snackbarService = inject(IdsSnackbarService);
   private readonly _viewContainerRef = inject(ViewContainerRef);
-
-  public snackbarVariant = SnackbarVariant;
+  private readonly _customActions: IdsSnackbarAction[] = [{ label: 'Log to console', action: this.action }];
+  protected _areSnackbarsOpen = computed(() => this._snackbarService.snackbars().length > 0);
 
   public ngOnInit(): void {
-    this._snackbarService.setViewContainerRef(this._viewContainerRef);
+    this.setActualViewContainer();
   }
 
-  public openSnackbar(
-    variant: SnackbarVariantType,
-    actions?: IdsSnackbarAction[],
-    allowDismiss?: boolean,
-    closeButtonLabel?: string,
-    autoClose?: boolean,
-  ): void {
-    const message: string = snackbarTexts[snackbarTextIndex];
-    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-    snackbarTextIndex < snackbarTexts.length - 1 ? snackbarTextIndex++ : snackbarTextIndex = 0;
+  public openSnackbar(): void {
     this._snackbarService.add({
-      message,
-      variant,
-      actions,
-      allowDismiss,
-      closeButtonLabel,
-      autoClose,
+      message: this.model.message,
+      variant: this.model.variant,
+      actions: this.model.useAction ? this._customActions : undefined,
+      allowDismiss: this.model.allowDismiss,
+      closeButtonLabel: this.model.closeButtonLabel,
+      autoClose: this.model.autoClose,
     });
   }
 
   public action(): void {
     console.info('action was called');
+  }
+
+  public setActualViewContainer(): void {
+    if (this.model.useActualViewContainer) {
+      this._snackbarService.setViewContainerRef(this._viewContainerRef);
+    } else {
+      this._snackbarService.clearViewContainerRef();
+    }
   }
 }

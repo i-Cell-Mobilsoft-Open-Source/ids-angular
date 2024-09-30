b51dc645ca780817f3b4e53ced6adecf49764c23
László Bátori <laszlo.batori@icellmobilsoft.hu>
2024-09-30 17:21:12 +0200

    feat(demo): segmented control toggle

--- projects/demo/src/app/pages/segmented-control-toggle/segmented-control-toggle-demo.component.ts
+++ projects/demo/src/app/pages/segmented-control-toggle/segmented-control-toggle-demo.component.ts
@@ -1,11 +1,25 @@
 import { UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { Size, SizeType } from '@i-cell/ids-angular/core';
 import { IdsIconComponent } from '@i-cell/ids-angular/icon';
 import { IdsSegmentedControlToggleDirective, IdsSegmentedControlToggleItemComponent, SegmentedControlAppearance, SegmentedControlAppearanceType, SegmentedControlVariant, SegmentedControlVariantType } from '@i-cell/ids-angular/segmented-control';
 import { mdiAccount, mdiAlarm, mdiLightbulbOnOutline } from '@mdi/js';
 import { TranslateModule } from '@ngx-translate/core';
+
+type SegmentedControlTogglePublicApi = {
+  size: SizeType,
+  variant: SegmentedControlVariantType,
+  appearance: SegmentedControlAppearanceType,
+  disabled: boolean,
+};
+
+type SegmentedControlToggleHelperControls = {
+  itemHasLabel: boolean,
+  itemHasIcon: boolean,
+  itemHasSuffix: boolean,
+  onlyOneItemIsDisabled: boolean,
+};
 
 @Component({
   selector: 'app-segmented-control-demo',
@@ -16,35 +30,33 @@ import { TranslateModule } from '@ngx-translate/core';
     IdsIconComponent,
     UpperCasePipe,
     FormsModule,
-    ReactiveFormsModule,
     TranslateModule,
   ],
   templateUrl: './segmented-control-toggle-demo.component.html',
-  styleUrl: './segmented-control-toggle-demo.component.scss',
+  styleUrls: [
+    '../demo-page.scss',
+    './segmented-control-toggle-demo.component.scss',
+  ],
 })
 export class SegmentedControlToggleDemoComponent {
+  public model: SegmentedControlTogglePublicApi & SegmentedControlToggleHelperControls = {
+    size: Size.COMFORTABLE,
+    variant: SegmentedControlVariant.SURFACE,
+    appearance: SegmentedControlAppearance.FILLED,
+    disabled: false,
+    itemHasLabel: true,
+    itemHasIcon: true,
+    itemHasSuffix: true,
+    onlyOneItemIsDisabled: false,
+  };
+
   public icon = {
     lightbulb: mdiLightbulbOnOutline,
     account: mdiAccount,
     alarm: mdiAlarm,
   };
 
-  public sizes: SizeType[] = [
-    Size.DENSE,
-    Size.COMPACT,
-    Size.COMFORTABLE,
-    Size.SPACIOUS,
-  ];
-
-  public variants: SegmentedControlVariantType[] = [
-    SegmentedControlVariant.PRIMARY,
-    SegmentedControlVariant.SURFACE,
-    SegmentedControlVariant.LIGHT,
-    SegmentedControlVariant.DARK,
-  ];
-
-  public appearances: SegmentedControlAppearanceType[] = [
-    SegmentedControlAppearance.FILLED,
-    SegmentedControlAppearance.OUTLINED,
-  ];
+  public sizes = Object.values(Size) as SizeType[];
+  public variants = Object.values(SegmentedControlVariant) as SegmentedControlVariantType[];
+  public appearances = Object.values(SegmentedControlAppearance) as SegmentedControlAppearanceType[];
 }

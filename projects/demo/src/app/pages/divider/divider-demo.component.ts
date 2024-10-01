1756b1b12c2ad7177901394831fe16b0b8ded705
László Bátori <laszlo.batori@icellmobilsoft.hu>
2024-10-01 15:04:19 +0200

    feat(demo): divider

--- projects/demo/src/app/pages/divider/divider-demo.component.ts
+++ projects/demo/src/app/pages/divider/divider-demo.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IdsButtonComponent } from '@i-cell/ids-angular/button';
 import {
   AllVariants,
   AllVariantsType,
   Size,
   SizeType,
-} from '@i-cell/ids-angular/core';
+  OrientationType, 
+  Orientation } from '@i-cell/ids-angular/core';
 import { IdsDividerComponent } from '@i-cell/ids-angular/divider';
 import { TranslateModule } from '@ngx-translate/core';
+
+type DividerPublicApi = {
+  orientation: OrientationType,
+  size: SizeType,
+  variant: AllVariantsType,
+  width: string,
+  height: string,
+};
 
 @Component({
   selector: 'app-divider-demo',
@@ -14,11 +25,30 @@ import { TranslateModule } from '@ngx-translate/core';
   imports: [
     IdsDividerComponent,
     TranslateModule,
+    FormsModule,
+    IdsButtonComponent,
   ],
   templateUrl: './divider-demo.component.html',
-  styleUrl: './divider-demo.component.scss',
+  styleUrls: [
+    '../demo-page.scss',
+    './divider-demo.component.scss',
+  ],
 })
 export class DividerDemoComponent {
+  public defaults: DividerPublicApi = {
+    orientation: Orientation.HORIZONTAL,
+    size: Size.COMFORTABLE,
+    variant: AllVariants.PRIMARY,
+    width: '100%',
+    height: '100%',
+  };
+
+  public model: DividerPublicApi = { ...this.defaults  };
+  public orientations = Object.values(Orientation) as OrientationType[];
   public sizes = Object.values(Size) as SizeType[];
   public variants = Object.values(AllVariants) as AllVariantsType[];
+
+  public reset(): void {
+    this.model = { ...this.defaults };
+  }
 }

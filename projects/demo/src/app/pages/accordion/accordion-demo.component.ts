9b23688885655cf8470499e9f5c8adeee699d3b9
László Bátori <laszlo.batori@icellmobilsoft.hu>
2024-10-01 17:00:05 +0200

    feat(demo): accordion

--- projects/demo/src/app/pages/accordion/accordion-demo.component.ts
+++ projects/demo/src/app/pages/accordion/accordion-demo.component.ts
@@ -1,7 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AccordionAppearance, AccordionAppearanceType, IdsAccordionComponent } from '@i-cell/ids-angular/accordion';
+import { IdsButtonComponent } from '@i-cell/ids-angular/button';
 import { Size, SizeType } from '@i-cell/ids-angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+
+type AccordionPublicApi = {
+  size: SizeType,
+  appearance: AccordionAppearanceType,
+  summary: string,
+  disabled: boolean,
+};
 
 @Component({
   selector: 'app-accordion-demo',
@@ -9,11 +18,29 @@ import { TranslateModule } from '@ngx-translate/core';
   imports: [
     IdsAccordionComponent,
     TranslateModule,
+    FormsModule,
+    IdsButtonComponent,
   ],
   templateUrl: './accordion-demo.component.html',
-  styleUrl: './accordion-demo.component.scss',
+  styleUrls: [
+    '../demo-page.scss',
+    './accordion-demo.component.scss',
+  ],
 })
 export class AccordionDemoComponent {
   public appearances = Object.values(AccordionAppearance) as AccordionAppearanceType[];
   public sizes = Object.values(Size) as SizeType[];
+
+  public defaults: AccordionPublicApi = {
+    appearance: AccordionAppearance.TEXT,
+    size: Size.COMFORTABLE,
+    summary: 'Summary text',
+    disabled: false,
+  };
+  
+  public model: AccordionPublicApi = { ...this.defaults };
+
+  public reset(): void {
+    this.model = { ...this.defaults };
+  }
 }

c7f2939c7b9691be91442748506cec7cd1766aaa
László Bátori <laszlo.batori@icellmobilsoft.hu>
2024-10-01 13:37:48 +0200

    feat(demo): icon

--- projects/demo/src/app/pages/icon/icon-demo.component.ts
+++ projects/demo/src/app/pages/icon/icon-demo.component.ts
@@ -1,9 +1,22 @@
 import { UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IdsButtonComponent } from '@i-cell/ids-angular/button';
 import { Size, SizeType } from '@i-cell/ids-angular/core';
-import { IconSizeCollection, IconSizeCollectionType, IconSource, IconVariant, IconVariantType } from '@i-cell/ids-angular/icon';
+import { IconSizeCollection, IconSizeCollectionType, IconSource, IconVariant, IconVariantType, IDS_ICON_DEFAULT_CONFIG_FACTORY } from '@i-cell/ids-angular/icon';
 import { IdsIconV2Component } from '@i-cell/ids-angular/icon/icon.component';
 import { TranslateModule } from '@ngx-translate/core';
+
+const defaultConfig = IDS_ICON_DEFAULT_CONFIG_FACTORY();
+
+type IconPublicApi = {
+  size: SizeType,
+  sizeCollection: IconSizeCollectionType,
+  variant: IconVariantType,
+  fontIcon: string
+  svgIconName: string,
+  ariaHidden: boolean,
+};
 
 @Component({
   standalone: true,
@@ -12,29 +25,30 @@ import { TranslateModule } from '@ngx-translate/core';
     IdsIconV2Component,
     UpperCasePipe,
     TranslateModule,
+    FormsModule,
+    IdsButtonComponent,
   ],
   templateUrl: './icon-demo.component.html',
-  styleUrls: ['./icon-demo.component.scss'],
+  styleUrls: [
+    '../demo-page.scss',
+    './icon-demo.component.scss',
+  ],
 })
 export class IconDemoComponent {
-  public sizes: SizeType[] = [
-    Size.DENSE,
-    Size.COMPACT,
-    Size.COMFORTABLE,
-    Size.SPACIOUS,
-  ];
+  public defaults: IconPublicApi = {
+    size: defaultConfig.size,
+    sizeCollection: defaultConfig.sizeCollection,
+    variant: defaultConfig.variant,
+    fontIcon: 'moon',
+    svgIconName: 'user',
+    ariaHidden: false,
+  };
 
-  public variants: IconVariantType[] = [
-    IconVariant.PRIMARY,
-    IconVariant.SECONDARY,
-    IconVariant.LIGHT,
-    IconVariant.DARK,
-    IconVariant.ERROR,
-    IconVariant.SUCCESS,
-    IconVariant.WARNING,
-    IconVariant.BRAND,
-    IconVariant.SURFACE,
-  ];
+  public model: IconPublicApi = { ...this.defaults  };
+  
+  public sizes = Object.values(Size) as SizeType[];
+  public sizeCollections = Object.values(IconSizeCollection) as IconSizeCollectionType[];
+  public variants = Object.values(IconVariant) as IconVariantType[];
 
   public sourceType = IconSource;
 
@@ -42,4 +56,8 @@ export class IconDemoComponent {
     IconSizeCollection.SMALL,
     IconSizeCollection.BIG,
   ];
+
+  public reset(): void {
+    this.model = { ...this.defaults };
+  }
 }

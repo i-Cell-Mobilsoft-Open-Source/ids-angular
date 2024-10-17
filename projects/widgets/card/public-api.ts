91888b438288ba149231ebf7aeb6264ab3f8fc74
Árpád Soós <arpad.soos@icellmobilsoft.hu>
2024-10-17 14:36:50 +0200

    feat(card): fix order

--- projects/widgets/card/public-api.ts
+++ projects/widgets/card/public-api.ts
@@ -1,3 +1,5 @@
+export * from './types/card-appearances.type';
+export * from './types/card-variant.type';
 export * from './card-body.directive';
 export * from './card-defaults';
 export * from './card-footer.directive';
@@ -6,5 +8,3 @@ export * from './card-media.directive';
 export * from './card-subtitle.directive';
 export * from './card-title.directive';
 export * from './card.component';
-export * from './types/card-appearances.type';
-export * from './types/card-variant.type';

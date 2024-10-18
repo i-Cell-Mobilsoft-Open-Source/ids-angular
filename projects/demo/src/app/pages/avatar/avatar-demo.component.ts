45fcce1d4cf1cfb36df86de7994ec7a0562ea808
László Bátori <laszlo.batori@icellmobilsoft.hu>
2024-10-18 12:44:45 +0200

    (feat:dynamic-demo-page-controls): implemented in accordion, avatar, divider (wip), icon (wip)

--- projects/demo/src/app/pages/avatar/avatar-demo.component.ts
+++ projects/demo/src/app/pages/avatar/avatar-demo.component.ts
@@ -1,55 +1,79 @@
+import { ControlTableComponent } from '../../components/control-table/control-table.component';
+import { TryoutComponent } from '../../components/tryout/tryout.component';
+
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { DemoControlConfig } from '@demo-types/demo-control.type';
+import { convertEnumToStringArray } from '@demo-utils/convert-enum-to-string-array';
+import { getDefaultFromDemoConfig } from '@demo-utils/get-defaults-from-demo-config';
 import { IdsAvatarVariantType, IDS_AVATAR_DEFAULT_CONFIG_FACTORY, IdsAvatarComponent, IdsAvatarImageDirective, IdsAvatarVariant } from '@i-cell/ids-angular/avatar';
-import { IdsButtonComponent } from '@i-cell/ids-angular/button';
 import {
-  IdsSize,
   IdsSizeType,
   IdsSizeCollectionType,
+  IdsSize,
   IdsSizeCollection,
 } from '@i-cell/ids-angular/core';
 import { IdsIconComponent } from '@i-cell/ids-angular/icon';
 import { TranslateModule } from '@ngx-translate/core';
 
-type AvatarPublicApi = {
+const defaultConfig = IDS_AVATAR_DEFAULT_CONFIG_FACTORY();
+
+type AvatarInputs = {
   initials: string,
   size: IdsSizeType,
   sizeCollection: IdsSizeCollectionType,
   variant: IdsAvatarVariantType,
 };
 
-const defaultConfig = IDS_AVATAR_DEFAULT_CONFIG_FACTORY();
-
 @Component({
   selector: 'app-avatar-demo',
   standalone: true,
   imports: [
+    TryoutComponent,
+    ControlTableComponent,
     IdsAvatarComponent,
     IdsIconComponent,
     IdsAvatarImageDirective,
     TranslateModule,
     FormsModule,
-    IdsButtonComponent,
   ],
   templateUrl: './avatar-demo.component.html',
-  styleUrls: [
-    '../demo-page.scss',
-    './avatar-demo.component.scss',
-  ],
+  styleUrl: './avatar-demo.component.scss',
 })
 export class AvatarDemoComponent {
-  public sizes = Object.values<IdsSizeType>(IdsSize);
-  public sizeCollections = Object.values<IdsSizeCollectionType>(IdsSizeCollection);
-  public variants = Object.values<IdsAvatarVariantType>(IdsAvatarVariant);
-
-  public defaults: AvatarPublicApi = {
-    initials: 'SJ',
-    size: defaultConfig.size,
-    sizeCollection: defaultConfig.sizeCollection,
-    variant: defaultConfig.variant,
+  protected _inputControlConfig: DemoControlConfig<AvatarInputs> = {
+    initials: {
+      description: 'Avatar initials.',
+      type: 'string',
+      default: '-',
+      demoDefault: 'SJ',
+    },
+    size: {
+      description: 'Avatar size.',
+      type: 'IdsSizeType',
+      default: defaultConfig.size,
+      control: 'select',
+      list: convertEnumToStringArray(IdsSize),
+    },
+    sizeCollection: {
+      description: 'Avatar sizeCollection.',
+      type: 'IdsSizeCollectionType',
+      default: defaultConfig.sizeCollection,
+      control: 'select',
+      list: convertEnumToStringArray(IdsSizeCollection),
+    },
+    variant: {
+      description: 'Avatar variant.',
+      type: 'IdsAvatarVariantType',
+      default: defaultConfig.variant,
+      control: 'select',
+      list: convertEnumToStringArray(IdsAvatarVariant),
+    },
   };
 
-  public model: AvatarPublicApi = { ...this.defaults };
+  public defaults = getDefaultFromDemoConfig<AvatarInputs>(this._inputControlConfig);
+
+  public model: AvatarInputs = { ...this.defaults };
 
   public reset(): void {
     this.model = { ...this.defaults };
--- projects/demo/src/app/pages/icon/icon-demo.component.ts
+++ projects/demo/src/app/pages/icon/icon-demo.component.ts
@@ -1,27 +1,35 @@
+import { ControlTableComponent } from '../../components/control-table/control-table.component';
+import { TryoutComponent } from '../../components/tryout/tryout.component';
+
 import { UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { DemoControlConfig } from '@demo-types/demo-control.type';
+import { convertEnumToStringArray } from '@demo-utils/convert-enum-to-string-array';
+import { getDefaultFromDemoConfig } from '@demo-utils/get-defaults-from-demo-config';
 import { IdsButtonComponent } from '@i-cell/ids-angular/button';
 import { IdsSize, IdsSizeCollection, IdsSizeCollectionType, IdsSizeType } from '@i-cell/ids-angular/core';
-import { IdsIconSource, IdsIconVariant, IdsIconVariantType, IDS_ICON_DEFAULT_CONFIG_FACTORY } from '@i-cell/ids-angular/icon';
+import { IdsIconVariant, IdsIconVariantType, IDS_ICON_DEFAULT_CONFIG_FACTORY } from '@i-cell/ids-angular/icon';
 import { IdsIconComponent } from '@i-cell/ids-angular/icon/icon.component';
 import { TranslateModule } from '@ngx-translate/core';
 
 const defaultConfig = IDS_ICON_DEFAULT_CONFIG_FACTORY();
 
-type IconPublicApi = {
+type IconInputs = {
   size: IdsSizeType,
   sizeCollection: IdsSizeCollectionType,
   variant: IdsIconVariantType,
   fontIcon: string
-  svgIconName: string,
-  ariaHidden: boolean,
+  svgIcon: string,
+  'aria-hidden': boolean,
 };
 
 @Component({
   standalone: true,
   selector: 'app-icon-demo',
   imports: [
+    TryoutComponent,
+    ControlTableComponent,
     IdsIconComponent,
     UpperCasePipe,
     TranslateModule,
@@ -29,34 +37,61 @@ type IconPublicApi = {
     IdsButtonComponent,
   ],
   templateUrl: './icon-demo.component.html',
-  styleUrls: [
-    '../demo-page.scss',
-    './icon-demo.component.scss',
-  ],
+  styleUrl: './icon-demo.component.scss',
 })
 export class IconDemoComponent {
-  public defaults: IconPublicApi = {
-    size: defaultConfig.size,
-    sizeCollection: defaultConfig.sizeCollection,
-    variant: defaultConfig.variant,
-    fontIcon: 'moon',
-    svgIconName: 'moon',
-    ariaHidden: false,
+  private _iconNames: string[] = [];
+
+  protected _inputControlConfig: DemoControlConfig<IconInputs> = {
+    size: {
+      description: 'Icon size.',
+      type: 'IdsSizeType',
+      default: defaultConfig.size,
+      control: 'select',
+      list: convertEnumToStringArray(IdsSize),
+    },
+    sizeCollection: {
+      description: 'Icon size collection.',
+      type: 'IdsSizeCollectionType',
+      default: defaultConfig.sizeCollection,
+      control: 'select',
+      list: convertEnumToStringArray(IdsSizeCollection),
+    },
+    variant: {
+      description: 'Icon variant.',
+      type: 'IdsIconVariantType',
+      default: defaultConfig.variant,
+      control: 'select',
+      list: convertEnumToStringArray(IdsIconVariant),
+    },
+    fontIcon: {
+      description: 'Name of font icon.',
+      type: 'string',
+      default: '-',
+      demoDefault: 'moon',
+      control: 'select',
+      list: this._iconNames,
+    },
+    svgIcon: {
+      description: 'Name of svg icon file',
+      type: 'IdsIconVariantType',
+      default: '-',
+      demoDefault: 'moon',
+      control: 'select',
+      list: this._iconNames,
+    },
+    'aria-hidden': {
+      description: 'Determinate whether the component is hidden or not for screen readers.',
+      type: 'boolean',
+      default: false,
+      control: 'checkbox',
+    },
   };
 
-  public model: IconPublicApi = { ...this.defaults  };
+  public defaults = getDefaultFromDemoConfig<IconInputs>(this._inputControlConfig);
+
+  public model: IconInputs = { ...this.defaults  };
   
-  public sizes = Object.values<IdsSizeType>(IdsSize);
-  public sizeCollections = Object.values<IdsSizeCollectionType>(IdsSizeCollection);
-  public variants = Object.values<IdsIconVariantType>(IdsIconVariant);
-
-  public sourceType = IdsIconSource;
-
-  public sizeCollectionType: IdsSizeCollectionType[] = [
-    IdsSizeCollection.SMALL,
-    IdsSizeCollection.BIG,
-  ];
-
   public reset(): void {
     this.model = { ...this.defaults };
   }

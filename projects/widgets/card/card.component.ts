55d5da85285588e175dfbdd84618d058f383a662
László Bátori <laszlo.batori@icellmobilsoft.hu>
2024-11-04 13:09:41 +0100

    (feat:componentbase-refact): card

--- projects/widgets/card/card.component.ts
+++ projects/widgets/card/card.component.ts
@@ -7,20 +7,18 @@ import {
   ChangeDetectionStrategy,
   Component,
   EventEmitter,
-  InjectionToken,
   OnInit,
   Output,
   ViewEncapsulation,
   computed,
-  inject,
   input,
   signal,
 } from '@angular/core';
 import {
+  ComponentBaseWithDefaults,
   IdsOrientationType,
   IdsSizeType,
   coerceBooleanAttribute,
-  createClassList,
 } from '@i-cell/ids-angular/core';
 
 const defaultConfig = IDS_CARD_DEFAULT_CONFIG_FACTORY();
@@ -42,8 +40,10 @@ const defaultConfig = IDS_CARD_DEFAULT_CONFIG_FACTORY();
     '[attr.tabindex]': ' this._hasClickHandler() ? 0 : null',
   },
 })
-export class IdsCardComponent implements OnInit {
-  private readonly _componentClass = 'ids-card';
+export class IdsCardComponent extends ComponentBaseWithDefaults<IdsCardDefaultConfig> implements OnInit {
+  protected override get _componentName(): string {
+    return 'card';
+  }
 
   protected readonly _defaultConfig = this._getDefaultConfig(defaultConfig, IDS_CARD_DEFAULT_CONFIG);
 
@@ -51,9 +51,7 @@ export class IdsCardComponent implements OnInit {
   public size = input<IdsSizeType>(this._defaultConfig.size);
   public variant = input<IdsCardVariantType>(this._defaultConfig.variant);
   public orientation = input<IdsOrientationType>(this._defaultConfig.orientation);
-  public disabled = input(false, {
-    transform: (value: boolean | string) => coerceBooleanAttribute(value),
-  });
+  public disabled = input(false, { transform: (value: boolean | string) => coerceBooleanAttribute(value) });
 
   // Old fashion output for now, signal outputs cannot be queried as of now for subscribers
   @Output()
@@ -62,26 +60,16 @@ export class IdsCardComponent implements OnInit {
 
   private _hasClickHandler = signal(false);
 
-  private _hostClasses = computed(() =>
-    createClassList(this._componentClass, [
-      this.appearance(),
-      this.size(),
-      this.variant(),
-      this.orientation(),
-      this.disabled() && this._hasClickHandler() ? 'disabled' : null,
-      this._hasClickHandler() ? 'clickable' : null,
-    ]),
-  );
+  protected _hostClasses = computed(() => this._getHostClasses([
+    this.appearance(),
+    this.size(),
+    this.variant(),
+    this.orientation(),
+    this.disabled() && this._hasClickHandler() ? 'disabled' : null,
+    this._hasClickHandler() ? 'clickable' : null,
+  ]));
 
   public ngOnInit(): void {
     this._hasClickHandler.set(this.click.observed);
-  }
-
-  // eslint-disable-next-line @stylistic/js/max-len
-  protected _getDefaultConfig(defaultConfig: Required<IdsCardDefaultConfig>, injectionToken: InjectionToken<IdsCardDefaultConfig>): Required<IdsCardDefaultConfig> {
-    return {
-      ...defaultConfig,
-      ...inject(injectionToken, { optional: true }),
-    };
   }
 }

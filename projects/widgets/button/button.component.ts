782fcef383f1d5be657fbc9d7902ad592b5e23b2
László Bátori <laszlo.batori@icellmobilsoft.hu>
2024-11-04 13:09:16 +0100

    (feat:componentbase-refact): button

--- projects/widgets/button/button.component.ts
+++ projects/widgets/button/button.component.ts
@@ -5,17 +5,15 @@ import { IdsButtonVariantType } from './types/button-variant.type';
 import {
   ChangeDetectionStrategy,
   Component,
-  InjectionToken,
   ViewEncapsulation,
   computed,
   contentChildren,
-  inject,
   input,
 } from '@angular/core';
 import {
+  ComponentBaseWithDefaults,
   IdsSizeType,
   coerceBooleanAttribute,
-  createClassList,
 } from '@i-cell/ids-angular/core';
 
 const defaultConfig = IDS_BUTTON_DEFAULT_CONFIG_FACTORY();
@@ -33,35 +31,24 @@ const defaultConfig = IDS_BUTTON_DEFAULT_CONFIG_FACTORY();
     '[attr.disabled]': 'this.disabled() ? "" : null',
   },
 })
-export class IdsButtonComponent {
-  private readonly _componentClass = 'ids-button';
+export class IdsButtonComponent extends ComponentBaseWithDefaults<IdsButtonDefaultConfig> {
+  protected override get _componentName(): string {
+    return 'button';
+  }
 
   protected readonly _defaultConfig = this._getDefaultConfig(defaultConfig, IDS_BUTTON_DEFAULT_CONFIG);
 
   public appearance = input<IdsButtonAppearanceType>(this._defaultConfig.appearance);
-
   public size = input<IdsSizeType>(this._defaultConfig.size);
   public variant = input<IdsButtonVariantType>(this._defaultConfig.variant);
-  public disabled = input(false, {
-    transform: (value: boolean | string) => coerceBooleanAttribute(value),
-  });
+  public disabled = input(false, { transform: (value: boolean | string) => coerceBooleanAttribute(value) });
 
   public iconLeading = contentChildren<unknown>('[icon-leading]');
   public iconTrailing = contentChildren<unknown>('[icon-trailing]');
 
-  private _hostClasses = computed(() =>
-    createClassList(this._componentClass, [
-      this.appearance(),
-      this.size(),
-      this.variant(),
-    ]),
-  );
-
-  // eslint-disable-next-line @stylistic/js/max-len
-  protected _getDefaultConfig(defaultConfig: Required<IdsButtonDefaultConfig>, injectionToken: InjectionToken<IdsButtonDefaultConfig>): Required<IdsButtonDefaultConfig> {
-    return {
-      ...defaultConfig,
-      ...inject(injectionToken, { optional: true }),
-    };
-  }
+  protected _hostClasses = computed(() => this._getHostClasses([
+    this.appearance(),
+    this.size(),
+    this.variant(),
+  ]));
 }

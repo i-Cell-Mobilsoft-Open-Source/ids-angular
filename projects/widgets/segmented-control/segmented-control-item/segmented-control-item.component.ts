660f8a183c3170141a666a1637a0795ae8c109e2
László Bátori <laszlo.batori@icellmobilsoft.hu>
2024-11-05 11:34:21 +0100

    (feat:componentbase-refact): segmented-control & -toggle

--- projects/widgets/segmented-control/segmented-control-item/segmented-control-item.component.ts
+++ projects/widgets/segmented-control/segmented-control-item/segmented-control-item.component.ts
@@ -1,12 +1,10 @@
-import { IdsSegmentedControlItemBase } from './segmented-control-item-base';
-
 import { IdsSegmentedControlDirective } from '../segmented-control.directive';
 import { IdsSegmentedControlItemChange } from '../types/segmented-control-item-change.class';
+import { SegmentedControlItem } from '../types/segmented-control-item.class';
 
-import { ChangeDetectionStrategy, Component, input, OnInit, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, ElementRef, inject, input, OnInit, output, signal, viewChild, ViewEncapsulation } from '@angular/core';
+import { addClassPrefix, coerceNumberAttribute, ComponentBase } from '@i-cell/ids-angular/core';
 import { IdsIconComponent } from '@i-cell/ids-angular/icon';
-
-let nextUniqueId = 0;
 
 @Component({
   selector: 'ids-segmented-control-item',
@@ -17,18 +15,55 @@ let nextUniqueId = 0;
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class IdsSegmentedControlItemComponent
-  extends IdsSegmentedControlItemBase<IdsSegmentedControlDirective, IdsSegmentedControlItemChange>
-  implements OnInit {
-  protected readonly _componentClass = 'ids-segmented-control-item';
-  protected readonly _uniqueId = `${this._componentClass}-${++nextUniqueId}`;
-
-  public id = input<string>(this._uniqueId);
-
-  protected _getParent(): IdsSegmentedControlDirective | null {
-    return this.injector.get(IdsSegmentedControlDirective, null, { optional: true, skipSelf: true });
+  extends ComponentBase
+  implements OnInit, SegmentedControlItem {
+  protected override get _hostName(): string {
+    return 'segmented-control-item';
   }
 
-  protected _createItemChangeEvent(): IdsSegmentedControlItemChange {
-    return new IdsSegmentedControlItemChange(this, !this.selected(), this.value());
+  protected readonly _group = inject(IdsSegmentedControlDirective, { optional: true, skipSelf: true });
+
+  private _buttonElement = viewChild.required<ElementRef<HTMLButtonElement>>('button');
+
+  public selected = signal<boolean>(false);
+
+  public name = input<string>();
+  public value = input.required<unknown>();
+  public label = input<string>();
+  public ariaLabel = input<string>('', { alias: 'aria-label' });
+  public ariaLabelledBy = input<string>('', { alias: 'aria-labeledby' });
+  public tabIndex = input<number, unknown>(0, { transform: coerceNumberAttribute });
+  public disabled = input<boolean>(false);
+
+  protected _buttonId = computed(() => `${this.id()}-button`);
+  protected _parentOrSelfDisabled = computed(() => this.disabled() || this._group?.disabled());
+  protected _multiSelect = computed(() => this._group?.multiSelect());
+  protected _ariaPressed = computed(() => (this._multiSelect() ? this.selected() : null));
+  protected _ariaChecked = computed(() => (!this._multiSelect() ? this.selected() : null));
+  protected _role = computed(() => (this._multiSelect() ? 'button' : 'radio'));
+  protected _buttonName = computed(() => (this._multiSelect() ? this.name() : this._group?.name()));
+  protected _buttonClasses = computed(() => addClassPrefix(this._hostClassName, this.selected() ? 'selected' : null));
+  protected _hostClasses = computed(() => this._getHostClasses([]));
+
+  public readonly changes = output<IdsSegmentedControlItemChange>();
+
+  public ngOnInit(): void {
+    if (!this._group) {
+      throw new Error(this._createHostError('component must be direct child of a segmented control'));
+    }
+
+    if (this._group.isItemPreSelectedByValue(this.value())) {
+      this.selected.set(true);
+    }
+  }
+
+  public onClick(): void {
+    if (this._multiSelect() || !this.selected()) {
+      this.changes.emit(new IdsSegmentedControlItemChange(this, !this.selected(), this.value()));
+    }
+  }
+
+  public focus(options?: FocusOptions): void {
+    this._buttonElement().nativeElement.focus(options);
   }
 }
--- projects/widgets/segmented-control/segmented-control.directive.ts
+++ projects/widgets/segmented-control/segmented-control.directive.ts
@@ -1,14 +1,21 @@
-import { IdsSegmentedControlBase } from './segmented-control-base';
+import { IDS_SEGMENTED_CONTROL_DEFAULT_CONFIG, IDS_SEGMENTED_CONTROL_DEFAULT_CONFIG_FACTORY, IdsSegmentedControlDefaultConfig } from './segmented-control-defaults';
 import { IdsSegmentedControlItemComponent } from './segmented-control-item/segmented-control-item.component';
+import { SegmentedControl } from './types/segmented-contol.class';
+import { IdsSegmentedControlAppearanceType } from './types/segmented-control-appearance.type';
 import { IdsSegmentedControlItemChange } from './types/segmented-control-item-change.class';
+import { IdsSegmentedControlVariantType } from './types/segmented-control-variant.type';
 
-import { contentChildren, Directive, EventEmitter, forwardRef, input, Output } from '@angular/core';
-import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import { SelectionModel } from '@angular/cdk/collections';
+import { AfterContentInit, computed, contentChildren, Directive, forwardRef, Input, input, isDevMode, OnInit, output, signal } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { ComponentBaseWithDefaults, IdsSizeType } from '@i-cell/ids-angular/core';
 
-let nextUniqueId = 0;
+const defaultConfig = IDS_SEGMENTED_CONTROL_DEFAULT_CONFIG_FACTORY();
 
 @Directive({
-  selector: 'ids-segmented-control[ngModel],ids-segmented-control[formControl],ids-segmented-control[formControlName]',
+  selector: `ids-segmented-control[ngModel],
+             ids-segmented-control[formControl],
+             ids-segmented-control[formControlName]`,
   standalone: true,
   providers: [
     {
@@ -18,21 +25,133 @@ let nextUniqueId = 0;
     },
   ],
   host: {
-    '[attr.id]': 'id()',
     '[attr.role]': 'multiSelect() ? "group" : "radiogroup"',
+    '(keydown)': '_handleKeyDown($event)',
   },
 })
-export class IdsSegmentedControlDirective extends IdsSegmentedControlBase<IdsSegmentedControlItemComponent, IdsSegmentedControlItemChange> {
-  protected readonly _componentClass = 'ids-segmented-control';
-  protected readonly _uniqueId = `${this._componentClass}-${++nextUniqueId}`;
+export class IdsSegmentedControlDirective
+  extends ComponentBaseWithDefaults<IdsSegmentedControlDefaultConfig>
+  implements
+    AfterContentInit,
+    OnInit,
+    ControlValueAccessor,
+    SegmentedControl<IdsSegmentedControlItemComponent, IdsSegmentedControlItemChange> {
+  protected override get _hostName(): string {
+    return 'segmented-control';
+  }
 
-  protected _items = contentChildren<IdsSegmentedControlItemComponent>(IdsSegmentedControlItemComponent);
-  public id = input<string>(this._uniqueId);
+  protected readonly _defaultConfig = this._getDefaultConfig(defaultConfig, IDS_SEGMENTED_CONTROL_DEFAULT_CONFIG);
+
+  private _selectionModel?: SelectionModel<IdsSegmentedControlItemComponent>;
+  private _rawValue: unknown | unknown[];
+  private _items = contentChildren<IdsSegmentedControlItemComponent>(IdsSegmentedControlItemComponent);
+
+  public name = input<string>();
+  public size = input<IdsSizeType>(this._defaultConfig.size);
+  public variant = input<IdsSegmentedControlVariantType>(this._defaultConfig.variant);
+  public appearance = input<IdsSegmentedControlAppearanceType>(this._defaultConfig.appearance);
   public multiSelect = input<boolean>(false);
+  public disabled = signal<boolean>(false);
 
-  @Output() public readonly itemChanges = new EventEmitter<IdsSegmentedControlItemChange>();
+  protected _hostClasses = computed(() => this._getHostClasses([
+    this.size(),
+    this.variant(),
+    this.appearance(),
+    this.disabled() ? 'disabled' : null,
+  ]));
 
-  protected _handleItemChanges(change: IdsSegmentedControlItemChange): void {
+  private _onChange: (value: unknown) => void = () => {};
+  private _onTouched: () => unknown = () => {};
+
+  @Input() public valueCompareFn?: (o1: IdsSegmentedControlItemComponent, o2: IdsSegmentedControlItemComponent) => boolean;
+
+  public readonly itemChanges = output<IdsSegmentedControlItemChange>();
+
+  private _handleKeyDown(event: KeyboardEvent): void {
+    // eslint-disable-next-line @stylistic/js/array-bracket-newline, @stylistic/js/array-element-newline
+    const navigationKeys = ['ArrowLeft', 'ArrowRight', 'Enter', ' '];
+
+    if (!navigationKeys.includes(event.key)) {
+      return;
+    }
+
+    event.preventDefault();
+
+    const items = this._items();
+    const target = event.target as HTMLButtonElement;
+    const buttonId = target.id;
+    const index = items.findIndex((item) => item.id() === buttonId);
+
+    switch (event.key) {
+      case 'ArrowLeft': {
+        if (index === 0) {
+          return;
+        }
+        const prevItem = items[index - 1];
+        prevItem.focus();
+        break;
+      }
+      case 'ArrowRight': {
+        if (index === (items.length - 1)) {
+          return;
+        }
+        const nextItem = items[index + 1];
+        nextItem.focus();
+        break;
+      }
+      case 'Enter':
+      case ' ': {
+        items[index].onClick();
+        break;
+      }
+      default:
+        return;
+    }
+  }
+
+  public ngOnInit(): void {
+    this._selectionModel = new SelectionModel<IdsSegmentedControlItemComponent>(this.multiSelect(), undefined, false, this.valueCompareFn);
+  }
+
+  public ngAfterContentInit(): void {
+    const items = this._items();
+    const minItemCount = 2;
+
+    if (isDevMode() && (items.length < minItemCount)) {
+      throw new Error(this._createHostError('invalid count of segmented control items. Minimum item count is 2.'));
+    }
+
+    this._selectionModel?.select(...this._items().filter((item) => item.selected()));
+    this._subscribeItemChanges();
+  }
+
+  public writeValue(value: unknown | unknown[]): void {
+    this._setSelectionByValue(value);
+  }
+
+  public registerOnChange(fn: () => void): void {
+    this._onChange = fn;
+  }
+
+  public registerOnTouched(fn: () => unknown): void {
+    this._onTouched = fn;
+  }
+
+  public setDisabledState?(isDisabled: boolean): void {
+    this.disabled.set(isDisabled);
+  }
+
+  private _subscribeItemChanges(): void {
+    this._items().forEach((item) => {
+      item.changes.subscribe(
+        (change) => {
+          this._handleItemChanges(change);
+        },
+      );
+    });
+  }
+
+  private _handleItemChanges(change: IdsSegmentedControlItemChange): void {
     const { source, selected } = change;
     if (!this.multiSelect()) {
       this._clearSelection();
@@ -46,5 +165,61 @@ export class IdsSegmentedControlDirective extends IdsSegmentedControlBase<IdsSeg
     this.itemChanges.emit(change);
     this._handleChange();
     this._onTouched();
+  }
+
+  private _setSelectionByValue(value: unknown | unknown[]): void {
+    this._rawValue = value;
+
+    if (this._items().length === 0) {
+      return;
+    }
+
+    if (this.multiSelect() && value) {
+      if (!Array.isArray(value)) {
+        throw new Error(this._createHostError('value must be an array in multiple-selection mode'));
+      }
+
+      this._clearSelection();
+      value.forEach((currentValue: unknown) => this._selectValue(currentValue));
+    } else {
+      this._clearSelection();
+      this._selectValue(value);
+    }
+  }
+
+  private _selectValue(value: unknown): void {
+    const correspondingItem = this._items().find((item) => item.value() != null && item.value() === value);
+    if (correspondingItem) {
+      correspondingItem.selected.set(true);
+      this._selectionModel?.select(correspondingItem);
+    }
+  }
+
+  private _clearSelection(): void {
+    this._selectionModel?.clear();
+    this._items().forEach((item) => {
+      item.selected.set(false);
+    });
+  }
+
+  private _handleChange(): void {
+    const selectionModelValues = this._selectionModel?.selected?.map((item) => item.value());
+    if (this.multiSelect()) {
+      this._onChange(selectionModelValues);
+    } else {
+      this._onChange(selectionModelValues?.[0]);
+    }
+  }
+
+  public isItemPreSelectedByValue(itemValue: unknown): boolean {
+    if (this._rawValue === undefined) {
+      return false;
+    }
+
+    if (this.multiSelect() && Array.isArray(this._rawValue)) {
+      return this._rawValue.some((value) => itemValue != null && value === itemValue);
+    }
+
+    return itemValue === this._rawValue;
   }
 }

1a9646fb9ec0605ac14fdd839487c1b5e2e18904
László Bátori <laszlo.batori@icellmobilsoft.hu>
2024-11-12 14:29:58 +0100

    (feat:selection-model-cdk): SelectionModel from CDK

--- projects/widgets/core/utils/create-error.ts
+++
@@ -1,4 +0,0 @@
-// TODO: delete this file after refactoring ComponentBase
-export function createComponentError(component: string, message: string): string {
-  return `${component}: ${message}`;
-}

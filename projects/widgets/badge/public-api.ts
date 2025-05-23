68a7b64031518f58a3e7955bcbaa7441015eb665
Vidóczi Péter <>
2025-05-23 08:29:29 +0200

    feat(badge) template fix

--- projects/widgets/badge/public-api.ts
+++ projects/widgets/badge/public-api.ts
@@ -2,3 +2,4 @@ export * from './badge.directive';
 export * from './types/badge-variant.type';
 export * from './types/badge-appearance.type';
 export * from './badge-defaults';
+export * from './badge-limit.pipe';

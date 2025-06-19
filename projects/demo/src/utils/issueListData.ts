5dd50fa34dac21013ef69c88c0ded6075a84cb99
andras varadi <andris@varadi.tv>
2025-06-19 08:25:29 +0200

    right url update

--- projects/demo/src/utils/issueListData.ts
+++ projects/demo/src/utils/issueListData.ts
@@ -5,11 +5,11 @@ export const ISSUE_DATA = [
     description: 'If you’d like to propose the design and development of something new feature or component please, submit a new request.',
     imageUrlLight: 'assets/images/issue-report/ids-request@2x.png',
     imageUrlDark: 'assets/images/issue-report/ids-request-dark@2x.png',
-    buttonLink: '#/components/accordion' },
+    buttonLink: 'https://icellmobilsoft-int.atlassian.net/jira/software/c/projects/IDS/form/10002' },
   { id: 2,
     title: 'Bug report',
     description: 'If you’ve encountered a design or development issue with an existing element, please, submit a bug report.',
     imageUrlLight: 'assets/images/issue-report/ids-bug@2x.png',
     imageUrlDark: 'assets/images/issue-report/ids-bug-dark@2x.png',
-    buttonLink: '#/components/action-menu' },
+    buttonLink: 'https://icellmobilsoft-int.atlassian.net/jira/software/c/projects/IDS/form/10134' },
 ];

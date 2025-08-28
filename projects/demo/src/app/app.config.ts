ebb516eec10a5ecbbcd81f3143a95e7d9bc09b85
Vidóczi Péter <>
2025-08-28 12:20:54 +0200

    feat(i18n): update app.config TranslateService

--- projects/demo/src/app/app.config.ts
+++ projects/demo/src/app/app.config.ts
@@ -1,19 +1,15 @@
 import { routes } from './app.routes';
 
-import { HttpClient, provideHttpClient, withInterceptors } from '@angular/common/http';
-import { ApplicationConfig, importProvidersFrom, inject } from '@angular/core';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { ApplicationConfig, inject } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter, withHashLocation } from '@angular/router';
 import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
 import { IDS_ICON_DEFAULT_CONFIG, IdsIconDefaultConfig } from '@i-cell/ids-angular/icon';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TRANSLATE_HTTP_LOADER_CONFIG, TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 import { provideApollo } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
-
-export function HttpLoaderFactory(): TranslateHttpLoader {
-  return new TranslateHttpLoader();
-}
 
 const iconDefaultConfig: IdsIconDefaultConfig = {
   iconAssetsPath: 'assets/icons',
@@ -21,28 +17,27 @@ const iconDefaultConfig: IdsIconDefaultConfig = {
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(withInterceptors([])), // Provide HTTP Client globally
+    provideHttpClient(withInterceptors([])),
     provideApollo((): ApolloClientOptions<unknown> => {
-      const httpLink = inject(HttpLink); // Correctly inject HttpLink
+      const httpLink = inject(HttpLink);
       return {
         cache: new InMemoryCache(),
         link: httpLink.create({
-          uri: 'https://ids.statamic.icellmobilsoft.hu/graphql', // Update with your Statamic GraphQL URL
+          uri: 'https://ids.statamic.icellmobilsoft.hu/graphql',
         }),
       };
     }),
     provideRouter(routes, withHashLocation()),
-    importProvidersFrom(
-      TranslateModule.forRoot({
-        loader: {
-          provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
-          deps: [HttpClient],
-        },
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({
+        prefix: './assets/i18n/',
+        suffix: '.json',
+        enforceLoading: false,
+        useHttpBackend: false,
       }),
-    ),
+      defaultLanguage: 'en',
+    }),
     provideAnimations(),
     { provide: IDS_ICON_DEFAULT_CONFIG, useValue: iconDefaultConfig },
-    { provide: TRANSLATE_HTTP_LOADER_CONFIG, useValue: {} },
   ],
 };

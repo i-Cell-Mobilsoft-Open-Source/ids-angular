20bd991fc864b3f9b5aa80f9d3bc17a4f1391485
Pipicz Petra <ppipicz24@gmail.com>
2025-10-22 12:19:53 +0200

    action-menu-demo refactor

---
+++ projects/demo/src/app/pages/action-menu/action-menu-demo-control.component.ts
@@ -0,0 +1,26 @@
+import { ActionMenuDemoService } from './action-menu-demo.service';
+
+import { ControlTableSmallComponent } from '../../components/control-table/control-table-small/control-table-small.component';
+import { TryoutControlComponent } from '../../components/tryout/tryout-controls.component';
+
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-action-menu-demo',
+  imports: [
+    TryoutControlComponent,
+    ControlTableSmallComponent,
+    TranslateModule,
+    FormsModule,
+  ],
+  templateUrl: './action-menu-demo-control.component.html',
+  styleUrls: [
+    '../demo-page.scss',
+    './action-menu-demo.component.scss',
+  ],
+})
+export class ActionMenuDemoControlComponent {
+  public demoService = inject(ActionMenuDemoService);
+}

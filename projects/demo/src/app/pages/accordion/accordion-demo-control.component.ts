8eb20649090028acec581977dc3a91cb83af55d8
Pipicz Petra <ppipicz24@gmail.com>
2025-10-27 13:08:55 +0100

    accordion demo done

--- projects/demo/src/app/pages/accordion/accordion-demo-control.component.ts
+++ projects/demo/src/app/pages/accordion/accordion-demo-control.component.ts
@@ -1,32 +1,11 @@
+import { AccordionDemoService } from './accordion-demo.service';
+
 import { ControlTableSmallComponent } from '../../components/control-table/control-table-small/control-table-small.component';
 import { TryoutControlComponent } from '../../components/tryout/tryout-controls.component';
 
-import { Component, viewChild, Output, EventEmitter } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { DemoControl, DemoControlConfig } from '@demo-types/demo-control.type';
-import { convertEnumToStringArray } from '@demo-utils/convert-enum-to-string-array';
-import { getDefaultFromDemoConfig } from '@demo-utils/get-defaults-from-demo-config';
-import { IDS_ACCORDION_DEFAULT_CONFIG_FACTORY, IdsAccordionAppearance, IdsAccordionAppearanceType, IdsAccordionComponent } from '@i-cell/ids-angular/accordion';
-import { IdsButtonAppearance, IdsButtonAppearanceType, IdsButtonVariant, IdsButtonVariantType } from '@i-cell/ids-angular/button';
-import { IdsSize, IdsSizeType } from '@i-cell/ids-angular/core';
 import { TranslateModule } from '@ngx-translate/core';
-
-const defaultConfig = IDS_ACCORDION_DEFAULT_CONFIG_FACTORY();
-
-type AccordionInputControls = {
-  size: IdsSizeType,
-  appearance: IdsAccordionAppearanceType,
-  summary: string,
-  disabled: boolean,
-  multi: boolean,
-  btnSize: IdsSizeType,
-  btnAppearance: IdsButtonAppearanceType,
-  btnVariant: IdsButtonVariantType,
-  expandBtnLabel: string,
-  collapseBtnLabel: string,
-  hasLeadingIcon: boolean,
-  hasTrailingIcon: boolean,
-};
 
 @Component({
   selector: 'app-accordion-demo-control',
@@ -43,99 +22,10 @@ type AccordionInputControls = {
   ],
 })
 export class AccordionDemoControlComponent {
-  private _accordion = viewChild<IdsAccordionComponent>('accordion');
-  protected _inputControlConfig: DemoControlConfig<AccordionInputControls> = {
-    size: {
-      description: 'Accordion size.',
-      type: 'IdsSizeType',
-      default: defaultConfig.size,
-      control: DemoControl.SELECT,
-      list: convertEnumToStringArray(IdsSize),
-    },
-    appearance: {
-      description: 'Accordion appearance.',
-      type: 'IdsAccordionAppearanceType',
-      default: defaultConfig.appearance,
-      control: DemoControl.SELECT,
-      list: convertEnumToStringArray(IdsAccordionAppearance),
-    },
-    summary: {
-      description: 'Summary of accordion',
-      type: 'string',
-      default: '-',
-      demoDefault: 'Summary text',
-    },
-    disabled: {
-      description: 'Whether the accordion is disabled or not.',
-      type: 'boolean',
-      default: false,
-      control: DemoControl.SWITCH,
-    },
-    multi: {
-      description: 'Allow multiple accordion items to be open.',
-      type: 'boolean',
-      default: false,
-      control: DemoControl.SWITCH,
-    },
-    btnSize: {
-      description: 'Button size.',
-      type: 'IdsSizeType',
-      default: defaultConfig.btnSize,
-      control: DemoControl.SELECT,
-      list: convertEnumToStringArray(IdsSize),
-    },
-    btnAppearance: {
-      description: 'Button appearance.',
-      type: 'IdsButtonAppearanceType',
-      default: defaultConfig.btnAppearance,
-      control: DemoControl.SELECT,
-      list: convertEnumToStringArray(IdsButtonAppearance),
-    },
-    btnVariant: {
-      description: 'Button variant.',
-      type: 'IdsButtonVariantType',
-      default: defaultConfig.btnVariant,
-      control: DemoControl.SELECT,
-      list: convertEnumToStringArray(IdsButtonVariant),
-    },
-    expandBtnLabel: {
-      description: 'Label for expand all button.',
-      type: 'string',
-      default: defaultConfig.expandBtnLabel,
-    },
-    collapseBtnLabel: {
-      description: 'Label for collapse all button.',
-      type: 'string',
-      default: defaultConfig.collapseBtnLabel,
-    },
-    hasLeadingIcon: {
-      description: 'Whether the button has leading icon or not.',
-      type: 'boolean',
-      default: defaultConfig.hasLeadingIcon,
-      control: DemoControl.SWITCH,
-    },
-    hasTrailingIcon: {
-      description: 'Whether the button has trailing icon or not.',
-      type: 'boolean',
-      default: defaultConfig.hasTrailingIcon,
-      control: DemoControl.SWITCH,
-    },
-  };
-
-  public defaults = getDefaultFromDemoConfig<AccordionInputControls>(this._inputControlConfig);
-
-  public model: AccordionInputControls = { ...this.defaults };
-
-  @Output() public modelChange = new EventEmitter<AccordionInputControls>();
-
-  public setModel(newModel: AccordionInputControls): void {
-    this.model = newModel;
-    this.modelChange.emit(this.model);
-  }
+  public accordionDemoService = inject(AccordionDemoService);
 
   public reset(): void {
-    this._accordion()?.closeAll();
-    this.model = { ...this.defaults };
+    this.accordionDemoService.reset();
   }
 
 }
--- projects/demo/src/app/pages/accordion/accordion-demo.component.ts
+++ projects/demo/src/app/pages/accordion/accordion-demo.component.ts
@@ -1,31 +1,12 @@
+import { AccordionDemoService } from './accordion-demo.service';
+
 import { TryoutComponent } from '../../components/tryout/tryout.component';
 
-import { Component, viewChild } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { DemoControl, DemoControlConfig } from '@demo-types/demo-control.type';
-import { convertEnumToStringArray } from '@demo-utils/convert-enum-to-string-array';
-import { getDefaultFromDemoConfig } from '@demo-utils/get-defaults-from-demo-config';
-import { IDS_ACCORDION_DEFAULT_CONFIG_FACTORY, IdsAccordionAppearance, IdsAccordionAppearanceType, IdsAccordionComponent, IdsAccordionItemComponent } from '@i-cell/ids-angular/accordion';
-import { IdsButtonAppearance, IdsButtonAppearanceType, IdsButtonVariant, IdsButtonVariantType } from '@i-cell/ids-angular/button';
-import { IdsSize, IdsSizeType } from '@i-cell/ids-angular/core';
+import { IdsAccordionComponent, IdsAccordionItemComponent } from '@i-cell/ids-angular/accordion';
 import { TranslateModule } from '@ngx-translate/core';
-
-const defaultConfig = IDS_ACCORDION_DEFAULT_CONFIG_FACTORY();
-
-type AccordionInputControls = {
-  size: IdsSizeType,
-  appearance: IdsAccordionAppearanceType,
-  summary: string,
-  disabled: boolean,
-  multi: boolean,
-  btnSize: IdsSizeType,
-  btnAppearance: IdsButtonAppearanceType,
-  btnVariant: IdsButtonVariantType,
-  expandBtnLabel: string,
-  collapseBtnLabel: string,
-  hasLeadingIcon: boolean,
-  hasTrailingIcon: boolean,
-};
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-accordion-demo',
@@ -42,92 +23,20 @@ type AccordionInputControls = {
     './accordion-demo.component.scss',
   ],
 })
-export class AccordionDemoComponent {
+export class AccordionDemoComponent implements OnInit, OnDestroy {
+  public accordionDemoService = inject(AccordionDemoService);
+
   private _accordion = viewChild<IdsAccordionComponent>('accordion');
-  protected _inputControlConfig: DemoControlConfig<AccordionInputControls> = {
-    size: {
-      description: 'Accordion size.',
-      type: 'IdsSizeType',
-      default: defaultConfig.size,
-      control: DemoControl.SELECT,
-      list: convertEnumToStringArray(IdsSize),
-    },
-    appearance: {
-      description: 'Accordion appearance.',
-      type: 'IdsAccordionAppearanceType',
-      default: defaultConfig.appearance,
-      control: DemoControl.SELECT,
-      list: convertEnumToStringArray(IdsAccordionAppearance),
-    },
-    summary: {
-      description: 'Summary of accordion',
-      type: 'string',
-      default: '-',
-      demoDefault: 'Summary text',
-    },
-    disabled: {
-      description: 'Whether the accordion is disabled or not.',
-      type: 'boolean',
-      default: false,
-      control: DemoControl.SWITCH,
-    },
-    multi: {
-      description: 'Allow multiple accordion items to be open.',
-      type: 'boolean',
-      default: false,
-      control: DemoControl.SWITCH,
-    },
-    btnSize: {
-      description: 'Button size.',
-      type: 'IdsSizeType',
-      default: defaultConfig.btnSize,
-      control: DemoControl.SELECT,
-      list: convertEnumToStringArray(IdsSize),
-    },
-    btnAppearance: {
-      description: 'Button appearance.',
-      type: 'IdsButtonAppearanceType',
-      default: defaultConfig.btnAppearance,
-      control: DemoControl.SELECT,
-      list: convertEnumToStringArray(IdsButtonAppearance),
-    },
-    btnVariant: {
-      description: 'Button variant.',
-      type: 'IdsButtonVariantType',
-      default: defaultConfig.btnVariant,
-      control: DemoControl.SELECT,
-      list: convertEnumToStringArray(IdsButtonVariant),
-    },
-    expandBtnLabel: {
-      description: 'Label for expand all button.',
-      type: 'string',
-      default: defaultConfig.expandBtnLabel,
-    },
-    collapseBtnLabel: {
-      description: 'Label for collapse all button.',
-      type: 'string',
-      default: defaultConfig.collapseBtnLabel,
-    },
-    hasLeadingIcon: {
-      description: 'Whether the button has leading icon or not.',
-      type: 'boolean',
-      default: defaultConfig.hasLeadingIcon,
-      control: DemoControl.SWITCH,
-    },
-    hasTrailingIcon: {
-      description: 'Whether the button has trailing icon or not.',
-      type: 'boolean',
-      default: defaultConfig.hasTrailingIcon,
-      control: DemoControl.SWITCH,
-    },
-  };
+  private _resetSubscription: Subscription | undefined;
 
-  public defaults = getDefaultFromDemoConfig<AccordionInputControls>(this._inputControlConfig);
+  public ngOnInit(): void {
+    this._resetSubscription = this.accordionDemoService.reset$.subscribe(() => {
+      this._accordion()?.closeAll();
+    });
+  }
 
-  public model: AccordionInputControls = { ...this.defaults };
-
-  public reset(): void {
-    this._accordion()?.closeAll();
-    this.model = { ...this.defaults };
+  public ngOnDestroy(): void {
+    // 5. Ne felejts el leiratkozni
+    this._resetSubscription?.unsubscribe();
   }
 }

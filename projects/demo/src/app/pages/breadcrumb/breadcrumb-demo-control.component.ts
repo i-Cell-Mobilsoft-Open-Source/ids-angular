58cf91d5a023440dfed7761cc4cd5454aae87597
Pipicz Petra <ppipicz24@gmail.com>
2025-10-27 13:37:14 +0100

    breadcrumb demo done

---
+++ projects/demo/src/app/pages/breadcrumb/breadcrumb-demo-control.component.ts
@@ -0,0 +1,20 @@
+import { BreadcrumbDemoService } from './breadcrumb-demo.service';
+
+import { ControlTableSmallComponent } from '../../components/control-table/control-table-small/control-table-small.component';
+import { TryoutControlComponent } from '../../components/tryout/tryout-controls.component';
+
+import { Component, inject } from '@angular/core';
+
+@Component({
+  selector: 'app-breadcrumb-demo',
+  standalone: true,
+  imports: [
+    TryoutControlComponent,
+    ControlTableSmallComponent,
+  ],
+  templateUrl: './breadcrumb-demo-control.component.html',
+  styleUrl: './breadcrumb-demo.component.scss',
+})
+export class BreadcrumbDemoControlComponent {
+  public breadcrumbDemoService = inject(BreadcrumbDemoService);
+}

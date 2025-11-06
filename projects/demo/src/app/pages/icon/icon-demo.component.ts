78987a1040520e2ab6f57a05f99fcf0b25603aa4
Pipicz Petra <ppipicz24@gmail.com>
2025-11-06 12:05:25 +0100

    icon demo done

--- projects/demo/src/app/pages/icon/icon-demo.component.ts
+++ projects/demo/src/app/pages/icon/icon-demo.component.ts
@@ -1,34 +1,16 @@
-import { ControlTableComponent } from '../../components/control-table/control-table.component';
-import { TryoutComponent } from '../../components/tryout/tryout.component';
-import { IconService } from '../../core/services/icon.service';
+import { IconDemoService } from './icon-demo.service';
 
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { TryoutComponent } from '../../components/tryout/tryout.component';
+
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { DemoControl, DemoControlConfig } from '@demo-types/demo-control.type';
-import { convertEnumToStringArray } from '@demo-utils/convert-enum-to-string-array';
-import { getDefaultFromDemoConfig } from '@demo-utils/get-defaults-from-demo-config';
-import { IdsSize, IdsSizeCollection, IdsSizeCollectionType, IdsSizeType } from '@i-cell/ids-angular/core';
-import { IdsIconVariant, IdsIconVariantType, IDS_ICON_DEFAULT_CONFIG_FACTORY } from '@i-cell/ids-angular/icon';
 import { IdsIconComponent } from '@i-cell/ids-angular/icon/icon.component';
 import { TranslateModule } from '@ngx-translate/core';
-
-const defaultConfig = IDS_ICON_DEFAULT_CONFIG_FACTORY();
-
-type IconInputControls = {
-  size: IdsSizeType,
-  sizeCollection: IdsSizeCollectionType,
-  variant: IdsIconVariantType,
-  fontIcon: string
-  svgIcon: string,
-  'aria-hidden': boolean,
-};
 
 @Component({
   selector: 'app-icon-demo',
   imports: [
     TryoutComponent,
-    ControlTableComponent,
     IdsIconComponent,
     TranslateModule,
     FormsModule,
@@ -40,76 +22,10 @@ type IconInputControls = {
   ],
 })
 export class IconDemoComponent implements OnInit {
-  private readonly _iconService = inject(IconService);
-  private readonly _destroyRef = inject(DestroyRef);
 
-  protected _inputControlConfig: DemoControlConfig<IconInputControls> = {
-    size: {
-      description: 'Icon size.',
-      type: 'IdsSizeType',
-      default: defaultConfig.size,
-      control: DemoControl.SELECT,
-      list: convertEnumToStringArray(IdsSize),
-    },
-    sizeCollection: {
-      description: 'Icon size collection.',
-      type: 'IdsSizeCollectionType',
-      default: defaultConfig.sizeCollection,
-      control: DemoControl.SELECT,
-      list: convertEnumToStringArray(IdsSizeCollection),
-    },
-    variant: {
-      description: 'Icon variant.',
-      type: 'IdsIconVariantType',
-      default: defaultConfig.variant,
-      control: DemoControl.SELECT,
-      list: convertEnumToStringArray(IdsIconVariant),
-    },
-    fontIcon: {
-      description: 'Name of font icon.',
-      type: 'string',
-      default: '-',
-      demoDefault: 'moon',
-      control: DemoControl.SELECT,
-      list: [],
-    },
-    svgIcon: {
-      description: 'Name of svg icon file',
-      type: 'IdsIconVariantType',
-      default: '-',
-      demoDefault: 'moon',
-      control: DemoControl.SELECT,
-      list: [],
-    },
-    'aria-hidden': {
-      description: 'Determinate whether the component is hidden or not for screen readers.',
-      type: 'boolean',
-      default: false,
-      control: DemoControl.CHECKBOX,
-    },
-  };
+  public iconDemoService = inject(IconDemoService);
 
   public ngOnInit(): void {
-    this._loadIcons();
-  }
-
-  private _loadIcons(): void {
-    this._iconService.loadIcons().pipe(
-      takeUntilDestroyed(this._destroyRef),
-    ).subscribe((list: string[]) => {
-      this._inputControlConfig = {
-        ...this._inputControlConfig,
-        fontIcon: { ...this._inputControlConfig.fontIcon, list },
-        svgIcon: { ...this._inputControlConfig.svgIcon, list },
-      };
-    });
-  }
-
-  public defaults = getDefaultFromDemoConfig<IconInputControls>(this._inputControlConfig);
-
-  public model: IconInputControls = { ...this.defaults  };
-
-  public reset(): void {
-    this.model = { ...this.defaults };
+    this.iconDemoService.loadIcons();
   }
 }
---
+++ projects/demo/src/app/pages/icon/icon-demo.service.ts
@@ -0,0 +1,100 @@
+import { IconService } from '../../core/services/icon.service';
+
+import { DestroyRef, inject, Injectable, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { DemoControl, DemoControlConfig } from '@demo-types/demo-control.type';
+import { convertEnumToStringArray } from '@demo-utils/convert-enum-to-string-array';
+import { getDefaultFromDemoConfig } from '@demo-utils/get-defaults-from-demo-config';
+import { IdsSize, IdsSizeCollection, IdsSizeCollectionType, IdsSizeType } from '@i-cell/ids-angular/core';
+import { IDS_ICON_DEFAULT_CONFIG_FACTORY, IdsIconVariant, IdsIconVariantType } from '@i-cell/ids-angular/icon';
+
+const defaultConfig = IDS_ICON_DEFAULT_CONFIG_FACTORY();
+
+type IconInputControls = {
+  size: IdsSizeType,
+  sizeCollection: IdsSizeCollectionType,
+  variant: IdsIconVariantType,
+  fontIcon: string
+  svgIcon: string,
+  'aria-hidden': boolean,
+};
+@Injectable()
+export class IconDemoService {
+  private readonly _iconService = inject(IconService);
+  private readonly _destroyRef = inject(DestroyRef);
+
+  public isLoaded = signal(false);
+
+  public inputControlConfig = signal<DemoControlConfig<IconInputControls>>({
+    size: {
+      description: 'Icon size.',
+      type: 'IdsSizeType',
+      default: defaultConfig.size,
+      control: DemoControl.SELECT,
+      list: convertEnumToStringArray(IdsSize),
+    },
+    sizeCollection: {
+      description: 'Icon size collection.',
+      type: 'IdsSizeCollectionType',
+      default: defaultConfig.sizeCollection,
+      control: DemoControl.SELECT,
+      list: convertEnumToStringArray(IdsSizeCollection),
+    },
+    variant: {
+      description: 'Icon variant.',
+      type: 'IdsIconVariantType',
+      default: defaultConfig.variant,
+      control: DemoControl.SELECT,
+      list: convertEnumToStringArray(IdsIconVariant),
+    },
+    fontIcon: {
+      description: 'Name of font icon.',
+      type: 'string',
+      default: '-',
+      demoDefault: 'search',
+      control: DemoControl.SELECT,
+      list: [],
+    },
+    svgIcon: {
+      description: 'Name of svg icon file',
+      type: 'IdsIconVariantType',
+      default: '-',
+      demoDefault: 'moon',
+      control: DemoControl.SELECT,
+      list: [],
+    },
+    'aria-hidden': {
+      description: 'Determinate whether the component is hidden or not for screen readers.',
+      type: 'boolean',
+      default: false,
+      control: DemoControl.SWITCH,
+    },
+  });
+
+  public loadIcons(): void {
+    this._iconService.loadIcons().pipe(
+      takeUntilDestroyed(this._destroyRef),
+    ).subscribe((list: string[]) => {
+
+      this.inputControlConfig.update((currentConfig) => ({
+        ...currentConfig,
+        fontIcon: { ...currentConfig.fontIcon, list: list },
+        svgIcon: { ...currentConfig.svgIcon, list: list },
+      }));
+
+      const currentDefaults = getDefaultFromDemoConfig<IconInputControls>(this.inputControlConfig());
+      this.model = { ...currentDefaults };
+
+      this.isLoaded.set(true);
+    });
+  }
+
+  public defaults = getDefaultFromDemoConfig<IconInputControls>(this.inputControlConfig());
+
+  public model: IconInputControls = { ...this.defaults  };
+
+  public reset(): void {
+    this.defaults = getDefaultFromDemoConfig<IconInputControls>(this.inputControlConfig());
+    this.model = { ...this.defaults };
+  }
+}

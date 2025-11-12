26a8985db4c03933c939e45433c2efe93d8c6aff
sanyisuveges <sanyi.suveges@gmail.com>
2025-11-12 18:08:13 +0000

    feat(dataTable): remove cellValue pre-sanitization

--- projects/widgets/table/directives/cell-renderer.ts
+++ projects/widgets/table/directives/cell-renderer.ts
@@ -5,30 +5,12 @@ import { isString, isTypeDerivedFrom } from '@i-cell/ids-angular/core';
 
 @Directive()
 export abstract class IdsTableCellRenderer<D> {
-  private readonly _entities = [
-    { rx: '&', entity: '&amp;' },
-    { rx: '<', entity: '&lt;' },
-    { rx: '>', entity: '&gt;' },
-    { rx: '"', entity: '&quot;' },
-    { rx: '\'', entity: '&#x27;' },
-    { rx: '/', entity: '&#x2F;' },
-  ];
-
-  public cellValue = input('', { transform: (value) => this._preSanitize(String(value ?? '')) });
+  public cellValue = input('', { transform: (value) => String(value ?? '') });
   public colDef = input.required<IdsTableColumnDef<D>>();
   public rowData = input<D>();
 
   protected _isComponentCellRenderer<C>(cellRendererDef: string | Type<C> | undefined):
   cellRendererDef is Type<C> {
     return !!cellRendererDef && !isString(cellRendererDef) && isTypeDerivedFrom(cellRendererDef, IdsTableCellRenderer);
-  }
-
-  protected _preSanitize(input: string): string {
-    if (!input) {
-      return input;
-    }
-
-    // Convert & to &amp;, Convert < to &lt;, Convert > to &gt;, Convert " to &quot;, Convert ' to &#x27;, Convert / to &#x2F;
-    return this._entities.reduce((result, item) => result.replaceAll(item.rx, item.entity), input);
   }
 }

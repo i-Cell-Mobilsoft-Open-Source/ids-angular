484767ae603dc4ad0ebcc06e2de5ecbdb62e19a9
Pipicz Petra <ppipicz24@gmail.com>
2026-04-08 13:58:55 +0200

    feat: Refactoring of contentCardData and the files that contain it

--- projects/demo/src/app/components/content-card/content-card.component.ts
+++ projects/demo/src/app/components/content-card/content-card.component.ts
@@ -44,7 +44,7 @@ export class ContentCardComponent  {
   }
 
   public imageData = computed(() => ({
-    transparent: this.contentCardData().imageBGTransparent === true,
+    transparent: this.contentCardData().image?.bgTransparent === true,
   }));
 
 }

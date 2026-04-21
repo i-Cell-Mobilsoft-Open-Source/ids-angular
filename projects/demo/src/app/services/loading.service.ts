3c160c8c61247697281ab68d25c6b6d6eaf39e01
Pipicz Petra <ppipicz24@gmail.com>
2026-04-21 12:51:50 +0200

    Moving the interceptor to a separate file

--- projects/demo/src/app/services/loading.service.ts
+++ projects/demo/src/app/services/loading.service.ts
@@ -1,6 +1,4 @@
-import { HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
-import { inject, Injectable, signal } from '@angular/core';
-import { finalize } from 'rxjs/operators';
+import { Injectable, signal } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class LoadingService {
@@ -15,18 +13,3 @@ export class LoadingService {
   }
 }
 
-export const loadingInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn) => {
-
-  //just the graphql requests should trigger the loading indicator, the rest of the requests are not relevant for the user
-  if (req.url.includes('/graphql')) {
-
-    const loadingService = inject(LoadingService);
-    loadingService.showSpinner();
-
-    return next(req).pipe(
-      finalize(() => loadingService.hideSpinner()),
-    );
-  }
-
-  return next(req);
-};

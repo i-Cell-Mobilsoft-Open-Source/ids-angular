0b3f450af1f32785adae00f8d5af558582932645
Pipicz Petra <ppipicz24@gmail.com>
2026-05-12 11:21:49 +0200

    feat(api-tab): add 'last update'

--- projects/demo/src/app/components/tabs/api/api.component.ts
+++ projects/demo/src/app/components/tabs/api/api.component.ts
@@ -1,10 +1,12 @@
 import { CURRENT_DEMO_SERVICE } from '../../../app.routes';
+import { ComponentDetailsComponent } from '../../../pages/components/component-details/component-details.component';
 import { MethodTableComponent } from '../../method-table/method-table.component';
 import { PropTableComponent } from '../../prop-table/prop-table.component';
 
 import { Component, inject, OnInit } from '@angular/core';
 import { DemoControlConfig } from '@demo-types/demo-control.type';
 import { DemoMethodConfig } from '@demo-types/demo-method.type';
+import { IdsChipComponent } from '@i-cell/ids-angular/chip';
 
 export interface IComponentDemoService {
   getApiConfig(): DemoControlConfig<unknown>[];
@@ -17,6 +19,7 @@ export interface IComponentDemoService {
   imports: [
     MethodTableComponent,
     PropTableComponent,
+    IdsChipComponent,
   ],
   templateUrl: './api.component.html',
 })
@@ -25,6 +28,9 @@ export class ApiComponent implements OnInit {
   protected _methodConfig: DemoMethodConfig[] = [];
 
   private _service = inject(CURRENT_DEMO_SERVICE);
+
+  private _componentDetails = inject(ComponentDetailsComponent);
+  protected _lastModified = this._componentDetails.lastModified;
 
   public ngOnInit(): void {
     this._methodConfig = this._service.getMethodConfig?.() ?? [];

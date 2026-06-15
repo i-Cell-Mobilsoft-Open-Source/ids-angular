efd69dba226d3a6005d3a75a97829ce111200bad
Pipicz Petra <ppipicz24@gmail.com>
2026-06-15 11:03:04 +0200

    feat(demo-and-code): add 'ts' view to examples, add tab to switch between html and ts

--- projects/demo/src/app/shared/ids-example-viewer/ids-example-viewer.component.ts
+++ projects/demo/src/app/shared/ids-example-viewer/ids-example-viewer.component.ts
@@ -1,5 +1,7 @@
 import { IdsExampleDef } from './ids-example.model';
 
+import { IdsTabGroupExtensionDirective } from '../../pages/components/component-details/ids-tab-group-extension.directive';
+
 import { Clipboard } from '@angular/cdk/clipboard';
 import { HttpClient } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component, computed, inject, input, OnInit, signal, viewChild, ViewContainerRef } from '@angular/core';
@@ -9,10 +11,16 @@ import { IdsCardComponent } from '@i-cell/ids-angular/card';
 import { IdsIconComponent } from '@i-cell/ids-angular/icon';
 import { IdsIconButtonComponent } from '@i-cell/ids-angular/icon-button';
 import { IdsSnackbarService } from '@i-cell/ids-angular/snackbar';
+import { IdsTabGroupComponent, IdsTabComponent } from '@i-cell/ids-angular/tab';
 import { IdsTooltipDirective } from '@i-cell/ids-angular/tooltip';
 import * as Prism from 'prismjs';
 import 'prismjs/components/prism-typescript';
 import { of, switchMap } from 'rxjs';
+
+interface NormalizedTab {
+  language: 'html' | 'ts';
+  path: string;
+}
 
 @Component({
   selector: 'ids-example-viewer',
@@ -24,6 +32,9 @@ import { of, switchMap } from 'rxjs';
     IdsIconButtonComponent,
     IdsIconComponent,
     IdsTooltipDirective,
+    IdsTabGroupComponent,
+    IdsTabComponent,
+    IdsTabGroupExtensionDirective,
   ],
 })
 export class IdsExampleViewerComponent implements OnInit {
@@ -32,24 +43,45 @@ export class IdsExampleViewerComponent implements OnInit {
   private _snackbar = inject(IdsSnackbarService);
   private _http = inject(HttpClient);
   private _dynamicHost = viewChild.required('dynamicHost', { read: ViewContainerRef });
-  private  _selectedFileIndex = signal(0);
-  private _currentFile = computed(() => this.example().files[this._selectedFileIndex()]);
-
-  protected _isCodeVisible = signal(false);
+  protected _selectedTabIndex = signal(0);
 
   public example = input.required<IdsExampleDef>();
+  protected _isCodeVisible = signal(false);
+
+  public readonly tabs = computed<NormalizedTab[]>(() => {
+    const normalized: NormalizedTab[] = [];
+    const filesArray = this.example().files;
+
+    if (!filesArray) {
+      return normalized;
+    }
+
+    for (const filePair of filesArray) {
+      if (filePair.HTMLpath) {
+        normalized.push({
+          language: 'html',
+          path: filePair.HTMLpath,
+        });
+      }
+      if (filePair.TSpath) {
+        normalized.push({
+          language: 'ts',
+          path: filePair.TSpath,
+        });
+      }
+    }
+    return normalized;
+  });
+
+  private readonly _currentTabPath = computed(() => this.tabs()[this._selectedTabIndex()]?.path ?? '');
 
   private readonly _rawCode = toSignal(
-    toObservable(this._currentFile).pipe(
-      switchMap((file) => {
-        if (!file) {
+    toObservable(this._currentTabPath).pipe(
+      switchMap((path) => {
+        if (!path) {
           return of('');
         }
-
-        if (file.HTMLpath) {
-          return this._http.get(file.HTMLpath, { responseType: 'text' });
-        }
-        return of('');
+        return this._http.get(path, { responseType: 'text' });
       }),
     ),
     { initialValue: '' },
@@ -57,14 +89,18 @@ export class IdsExampleViewerComponent implements OnInit {
 
   protected _highlightedCode = computed(() => {
     const code = this._rawCode();
-
     if (!code) {
       return '';
     }
 
-    const htmlString = Prism.highlight(code, Prism.languages['markup'], 'markup');
+    const currentTab = this.tabs()[this._selectedTabIndex()];
+    if (!currentTab) {
+      return '';
+    }
 
-    return this._sanitizer.bypassSecurityTrustHtml(htmlString);
+    const prismLang = currentTab.language === 'ts' ? 'typescript' : 'markup';
+    const highlighted = Prism.highlight(code, Prism.languages[prismLang], prismLang);
+    return this._sanitizer.bypassSecurityTrustHtml(highlighted);
   });
 
   public ngOnInit(): void {
@@ -79,7 +115,11 @@ export class IdsExampleViewerComponent implements OnInit {
 
   public toggleCode(): void {
     this._isCodeVisible.update((visible) => !visible);
-    this._selectedFileIndex.set(0);
+    this._selectedTabIndex.set(0);
+  }
+
+  public onTabChange(index: number): void {
+    this._selectedTabIndex.set(index);
   }
 
   public copyCode(): void {

cac5b906a3c8226b8c4a00e1aa13853d1c31323c
Laszlo Ilonka <laszlo.ilonka@icellmobilsoft.hu>
2026-09-10 14:07:43 +0200

    feat(switch): add samples
    IDS-2339

--- projects/demo/src/app/pages/switch/switch-demo.component.ts
+++ projects/demo/src/app/pages/switch/switch-demo.component.ts
@@ -4,6 +4,8 @@ import { ControlTableComponent } from '../../components/control-table/control-ta
 import { DemoAndCodeComponent } from '../../components/tabs/demo-and-code/demo-and-code.component';
 import { TryoutControlComponent } from '../../components/tryout/tryout-controls.component';
 import { TryoutComponent } from '../../components/tryout/tryout.component';
+import { SWITCH_EXAMPLES } from '../../components-example/switch/switch-examples';
+import { IdsExampleViewerComponent } from '../../shared/ids-example-viewer/ids-example-viewer.component';
 
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -21,6 +23,7 @@ import { TranslateModule } from '@ngx-translate/core';
     DemoAndCodeComponent,
     TryoutControlComponent,
     ControlTableComponent,
+    IdsExampleViewerComponent,
   ],
   templateUrl: './switch-demo.component.html',
   styleUrls: [
@@ -30,4 +33,5 @@ import { TranslateModule } from '@ngx-translate/core';
 })
 export class SwitchDemoComponent {
   protected _switchDemoService = inject(SwitchDemoService);
+  public readonly switchExamples = SWITCH_EXAMPLES;
 }
